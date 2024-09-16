8194b7c0ae04d7a9634ddfca7fd4f49ef8f88748
Prohor722 <prohor.b722@gmail.com>
2024-09-17 03:04:47 +0600

    simple factory pattern done!

--- EmployeeFactory.php
+++ EmployeeFactory.php
@@ -3,19 +3,18 @@ require 'Engineer.php';
 require 'Stuff.php';
 
 class EmployeeFactory{
-    public static function addEmployee($type, $name, $age, $salary, $post){
-        if($type == 'engineer'){
+    public static function addEmployee($type, $name='', $age=0, $salary=0, $post=''){
+        if($type==null){
+            return "Need Employee Data!";
+        }
+        elseif($type == 'engineer'){
             return new Engineer($name, $age, $salary, $post);
         }
         elseif($type == 'stuff'){
             return new Stuff($name, $age, $salary, $post);
         }
+        else{
+            return "Please check type!!";
+        }
     }
 }
-
-$emp1 = EmployeeFactory::addEmployee('engineer','Belal', 25, 26000, 1);
-$stuff1 = EmployeeFactory::addEmployee('stuff','Kurbot', 22, 12000, 'Care-Taker');
-
-echo $emp1->getEngeerDetails();
-echo "\r\n\r\n";
-echo $stuff1->getStuffDetails();
