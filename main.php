02b50d3191a3664fe6ca371a4a143479ca37c60b
Prohor722 <prohor.b722@gmail.com>
2024-09-17 04:19:26 +0600

    upload

--- main.php
+++ main.php
@@ -3,10 +3,8 @@
 
     $emp1 = EmployeeFactory::addEmployee('engineer','Belal', 25, 26000, 1);
     $stuff1 = EmployeeFactory::addEmployee('stuff','Kurbot', 22, 12000, 'Care-Taker');
-    // $test1 = EmployeeFactory::addEmployee();
     
     echo $emp1->getEngeerDetails();
     echo "\r\n\r\n";
     echo $stuff1->getStuffDetails();
-    // echo "\r\n\r\n";
 ?>
